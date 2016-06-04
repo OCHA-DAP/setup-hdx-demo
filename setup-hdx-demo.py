384ad086068ffddf04554001c1b7558bb08b9b29
David Megginson <david.megginson@megginson.com>
2016-06-04 16:49:34 -0400

    Fix typo.

--- setup-hdx-demo.py
+++ setup-hdx-demo.py
@@ -4,38 +4,34 @@ Started 2016-06-04 by David Megginson
 """
 
 import ckanapi
-import urllib
-import pprint
-
-# read configuration values from config.py
 import config
 
-#
-# Create the CKAN API object
-#
 ckan = ckanapi.RemoteCKAN(config.CONFIG['ckanurl'], apikey=config.CONFIG['apikey'], user_agent=config.CONFIG.get('user_agent', None))
+"""Remote CKAN API object"""
 
-#
-# Delete old Foolandia datasets (and create Foolandia if necessary)
-#
-try:
-    print("Try deleting any old Foolandia datasets...")
-    foo = ckan.action.group_show(id='foo') # will fail if it doesn't exist
-    # delete and purge any datasets belonging to Foolandia
-    for dataset in foo.get('packages'):
-        print("  ... deleting dataset " + dataset['name'])
-        ckan.action.package_purge(id=dataset['id'])
-except:
-    print("Foolandia did not exist ... creating")
-    ckan.action.group_create(
-        id='foo',
-        name='foo',
-        title='Foolandia (demo)',
-        display_name='Foolandia (demo)',
-        description='Fake country for training and demos'
-    )
-    print("  ... created new, empty Foolandia")
 
+def reset_group (group):
+    """If the group exists, delete all datasets; otherwise, create it."""
+    try:
+        print("Deleting all datasets in group {} ({}) ...".format(group['id'], group['title']))
+        group = ckan.action.group_show(id=group['id']) # will fail if it doesn't exist
+        for dataset in group.get('packages'):
+            print("  ... deleting dataset {}".format(dataset['name']))
+            ckan.action.package_purge(id=dataset['id'])
+    except:
+        print("Group {} ({}) did not exist: creating an empty version ...".format(group['id'], group['title']))
+        ckan.call_action('group_create', group)
+        print("  ... created new, empty group")
+
+
+# Reset the Foolandia (foo) demo country
+reset_group({
+    'id': 'foo',
+    'name': 'foo',
+    'title': 'Foolandia',
+    'display_name': 'Foolandia (demo)',
+    'description': 'Fake country for training and demos'
+})
 
 exit(0)
 
