0790d0993b03aca3ddfb3d2e0527f08d7aa62b8d
David Megginson <david.megginson@megginson.com>
2016-06-04 16:57:36 -0400

    Factor out dataset-deletion code into its own function.

--- setup-hdx-demo.py
+++ setup-hdx-demo.py
@@ -9,15 +9,20 @@ import config
 ckan = ckanapi.RemoteCKAN(config.CONFIG['ckanurl'], apikey=config.CONFIG['apikey'], user_agent=config.CONFIG.get('user_agent', None))
 """Remote CKAN API object"""
 
+def clear_datasets (entity):
+    """Delete all datasets associated with an entity (such as a group or organization)."""
+    datasets = entity.get('packages')
+    if datasets:
+        for dataset in datasets:
+            print("Deleting dataset {}".format(dataset['name']))
+            ckan.action.package_purge(id=dataset['id'])
 
 def reset_group (group):
     """If the group exists, delete all datasets; otherwise, create it."""
     try:
         print("Deleting all datasets in group {} ({}) ...".format(group['id'], group['title']))
         group = ckan.action.group_show(id=group['id']) # will fail if it doesn't exist
-        for dataset in group.get('packages'):
-            print("  ... deleting dataset {}".format(dataset['name']))
-            ckan.action.package_purge(id=dataset['id'])
+        clear_datasets(group)
     except:
         print("Group {} ({}) did not exist: creating an empty version ...".format(group['id'], group['title']))
         ckan.call_action('group_create', group)
