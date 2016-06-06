612f57427355b1de823a01d904788db03c5f3df6
David Megginson <david.megginson@megginson.com>
2016-06-06 16:33:30 -0400

    Sort-of working.

--- setup-hdx-demo.py
+++ setup-hdx-demo.py
@@ -20,13 +20,33 @@ def clear_datasets (entity):
 def reset_group (group):
     """If the group exists, delete all datasets; otherwise, create it."""
     try:
-        print("Deleting all datasets in group {} ({}) ...".format(group['id'], group['title']))
+        print("Deleting all datasets in group {} ({}) ...".format(group['name'], group['title']))
         group = ckan.action.group_show(id=group['id']) # will fail if it doesn't exist
         clear_datasets(group)
     except:
-        print("Group {} ({}) did not exist: creating an empty version ...".format(group['id'], group['title']))
+        print("Group {} ({}) did not exist: creating an empty version ...".format(group['name'], group['title']))
         ckan.call_action('group_create', group)
         print("  ... created new, empty group")
+
+def reset_user (user):
+    """If the user does not exist, create it; otherwise, update."""
+    try:
+        print("Creating or updating {} ({})".format(user['name'], user['fullname']))
+        ckan.call_action('user_create', user)
+    except:
+        print("User {} already exists: updating".format(user['name']))
+        #ckan.call_action('user_update', user)
+
+def reset_organization (org):
+    """If the org exists, delete all datasets; otherwise, create it."""
+    try:
+        print("Deleting all datasets in organization {} ({}) ...".format(org['name'], org['title']))
+        org = ckan.action.organization_show(id=org['name']) # will fail if it doesn't exist
+        clear_datasets(org)
+    except:
+        print("Organization {} ({}) did not exist: creating an empty version ...".format(org['name'], org['title']))
+        ckan.call_action('organization_create', org)
+        print("  ... created new, empty organization")
 
 
 # Reset the Foolandia (foo) demo country
@@ -37,6 +57,32 @@ reset_group({
     'display_name': 'Foolandia (demo)',
     'description': 'Fake country for training and demos'
 })
+
+for n in range(1, 21):
+    org_id = "demo_org_{}".format(n)
+    admin_id = "demo_user_{}a".format(n)
+    editor_id = "demo_user_{}b".format(n)
+    reset_user({
+        'name': admin_id,
+        'email': "{}@example.org".format(admin_id),
+        'password': 'hdxdemo',
+        'fullname': 'Demo User {}a'.format(n)
+    })
+    reset_user({
+        'name': editor_id,
+        'email': "{}@example.org".format(editor_id),
+        'password': 'impact2016',
+        'fullname': 'Demo User {}b'.format(n)
+    })
+    reset_organization({
+        'name': org_id,
+        'title': 'Demo Org {}'.format(n),
+        'description': 'Temporary organization for demonstrations and tutorials',
+        'users': [
+            {'name': admin_id, 'capacity': 'admin'},
+            {'name': editor_id, 'capacity': 'editor'}
+        ]
+    })
 
 exit(0)
 
